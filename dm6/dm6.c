19585b51414db607598086fc494dd6d4c0a92f0e
LesH2O <lacommeleopaul@gmail.com>
2025-01-09 19:50:15 +0100

    création de fichier usuelle + ex50 en .c

--- dm6/dm6.c
+++ dm6/dm6.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 
 int tables(int n){
 	if(n<6){exit(0);}
-	int* a = n/4 ;
-	int* r = n-(4*(*a)) ;
+	int a = n/4 ;
+	int r = n-(4*(a)) ;
 	while(r % 3 != 0){
-		*a -= 1 ;
-		*r += 4 ;
+		a -= 1 ;
+		r += 4 ;
 	}
-	int* b = *r/3 ;
+	int b = r /3 ;
 	return (a,b);
 }
 
-int* main50(){
+int main50(){
 	int n = 7 ;
-	
+	if(tables(n)==(1,1)){return 1;}
+	else{return 0;}
 }
 
 
 
 void main(){
-	exit(0);
+	int r = main50() ;
+	exit(r);
 }

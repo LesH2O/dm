771cc497fedb0e690e55d49117dbe41ef3f35132
LesH2O <lacommeleopaul@gmail.com>
2025-01-15 16:53:59 +0100

    correction ex51

--- dm6/dm6.c
+++ dm6/dm6.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 int tables(int n){
 	if(n<6){exit(0);}
@@ -20,53 +21,45 @@ int main50(){
 }
 
 double q1 (double* tab, int taille){
-	double* res = malloc(taille/4*sizeof(double));
-	int n = taille/4;
-	int s = 0;
-	if(n==0){exit(1);}
-	else{res[0] = tab[0];}
-	for(int i=1 ; i < taille ; i++)
-	{
-		if(s<n-1)
-		{
-			int j = s ;
-			s +=1 ;
-			res[j] = tab[i];
-    		while (j > 0 && res[j] < res[j-1])
-			{
-   				double buff = res[j];
-	   			res[j] = res[j-1];
-   				res[j-1] = buff;
-   				j -= 1;
-	    	}	
-		}
-		else if(tab[i]<res[s])
-		{
-			int j = s ;
-			res[j] = tab[i];
-    		while (j > 0 && res[j] < res[j-1])
-			{
-   				double buff = res[j];
-	   			res[j] = res[j-1];
-   				res[j-1] = buff;
-   				j -= 1;
-	    	}	
-		}
+	int seuils[2] = { 0, taille-1 };
+	bool trie = false;
+	int cro = 1;
+	double buff;
+	while (!trie && seuils[0] < seuils[1])
+  	{
+    	trie = true;
+    	for (int i = seuils[(1-cro)/2] ; i != seuils[(cro+1)/2] ; i += cro)
+    	{
+      		if (tab[i]*cro > tab[i+cro]*cro)
+      		{
+        		buff = tab[i];
+        		tab[i] = tab[i+cro];
+		        tab[i+cro] = buff;
+        		trie = false;
+	      }
+    	}
+	    seuils[(cro+1)/2] -= cro;
+    	cro *= -1;
 	}
-	double r = res[s] ;
-	free(res);
-	return r;
+	int n = taille/4 ;
+	return tab[n];
 }
 
 int main51(){
 	double tab[5] = {5.2,4.3, 2.9,1.8, 2.6};
 	double r = q1(tab,5) ;
-	if(r == 1.8){return(0) ;}
+	printf("%f \n",r);
+	if(r == 2.6){return(0) ;}
 	else{return(1);}
 }
 
+int main52(){
+	
+
+
+}
 
 void main(){
 	int r = main51() ;
-	exit(r);
+	exit(r) ;
 }

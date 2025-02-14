ded3e739190668e2676b013e897049e4da796f07
Cycynobi <cyanndsb@yahoo.com>
2025-02-14 21:02:27 +0100

    exercices 62/63 en ml

--- dm7/dm7.ml
+++ dm7/dm7.ml
@@ -36,3 +36,48 @@ let existe_somme t =
 existe_somme [|0;0;0|];;
 existe_somme [|1;2;4|];;
 existe_somme [|1;3;2|];;
+
+
+
+
+let fin_commune str1 str2 =
+  let n = ref 0 in
+  let len1 = String.length str1 in
+  let len2 = String.length str2 in
+  let max = (if len1>len2 then len2 else len1) in
+  let i = ref 1 in
+  let egaux = ref 1 in
+  while !egaux=1 && !i<max do (
+    if str1.[len1- !i]=str2.[len2- !i] then n := !n+1
+    else egaux:= 0;
+    i := !i+1;)
+  done; 
+  !n;;
+
+fin_commune "coucou" "j'ai mal au cou";;
+fin_commune "never" "give you up";;    
+
+
+
+
+type fraction =  { num : int; den : int};;
+
+let simplifiable t =
+  let n = Array.length t in
+  let res = ref false in
+  for i =1 to (n-1) do 
+    if (t.(i)).den = 0 then failwith "division par 0"
+    else (
+      for j = 0 to (i-1) do (
+        if (t.(j)).den = 0 then failwith "division par 0"
+        else ( if ((t.(i)).den)*((t.(j)).num)=((t.(j)).den)*((t.(i)).num) then res := true
+               else res := !res))
+      done;)
+  done;
+  !res;;
+
+
+      
+    
+
+
